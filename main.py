638d3162c206606a05c552b85b745d98f41a9772
Ahmed Kayungilo <ahmedkayungilo@gmail.com>
2022-08-01 15:54:35 +0300

    Completed the landing page and stock page without the chart

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+
+@app.route("/user")
+def landing():
+    return render_template('landingpage.html')
+
+
+@app.route('/stock')
+def stock():
+    return render_template('stock.html')
 
 
 if __name__ == "__main__":
